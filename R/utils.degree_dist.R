1f23293c000628db1f330b34e935804abad37b58
pat-alt <altmeyerpat@gmail.com>
2021-03-25 11:10:43 +0100

    start on report

--- R/utils.degree_dist.R
+++ R/utils.degree_dist.R
@@ -1,5 +1,9 @@
-degree_dist <- function(G, plot=TRUE) {
-  d <- diag(crossprod(G))
+degree_dist <- function(G, plot=TRUE, mode="in") {
+  if (mode=="in") {
+    d <- t(G)%*%rep(1,nrow(G))
+  } else {
+    d <- G%*%rep(1,nrow(G))
+  }
   degree_dist <- table(d)/length(d)
   if (plot) {
     barplot(degree_dist)
--- R/utils.erdos_renyi.R
+++ R/utils.erdos_renyi.R
@@ -1,5 +1,5 @@
 erdos_renyi <- function(n=100,p=0.02) {
-  g_ij <- rbinom((n*(n-1)/2),c(0,1),prob = p)
+  g_ij <- rbinom((n*(n-1)/2),1,prob = p)
   G <- matrix(rep(0,n^2),n)
   G[lower.tri(G)] <- g_ij
   G <- G+t(G)
